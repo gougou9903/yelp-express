07bc566c0eb6ffa02469430ee524a9a7fe23a8e2
Haoran Fang <gougou9903@gmail.com>
2015-02-08 03:55:28 -0700

    50/50 individual yelp-express by Haoran

--- search.js
+++ search.js
@@ -2,6 +2,7 @@ var doctors = require('./data/doctors.json')
 var restaurants = require('./data/restaurants.json')
 var users = require('./data/users.json')
 var tips = require('./data/tips.json')
+var _ = require('lodash')
 
 module.exports = function(app) {
 
@@ -14,8 +15,16 @@ module.exports = function(app) {
 
 
         // TODO: lookup restaurants whose names contain the given keyword
-        var rs = [restaurants[6], restaurants[10]] // hardcoded for 'Pizza'
+        
+        //var rs = [restaurants[6], restaurants[10]] // hardcoded for 'Pizza'
+        var rs = _.filter(restaurants,function(chr){
+            if(chr.name.indexOf(keyword) == -1)
+                return false
+            else
+                return true
 
+        });
+        
         res.render('listRestaurants.jade', {
             restaurants: rs
         })
@@ -25,7 +34,20 @@ module.exports = function(app) {
         var x = req.params.x
 
         // TODO: lookup restaurants good for  :x
-        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
+        //var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
+        var rs = _.filter(restaurants, function(chr){
+            for(var i in chr.attributes){
+                if(i == "Good For"){
+                    
+                    for(var j in chr.attributes[i]){
+                        if(j == x){
+                            
+                            return chr.attributes[i][j]}
+                    }
+                }
+            }
+            return false
+        });
 
         res.render('listRestaurants.jade', {
             restaurants: rs
@@ -36,7 +58,21 @@ module.exports = function(app) {
         var x = req.params.x
 
         // TODO: lookup restaurants has ambience of :x
-        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
+        //var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
+
+        var rs = _.filter(restaurants, function(chr){
+            for(var i in chr.attributes){
+                if(i == "Ambience"){
+                    
+                    for(var j in chr.attributes[i]){
+                        if(j == x){
+                            
+                            return chr.attributes[i][j]}
+                    }
+                }
+            }
+            return false
+        });
 
         res.render('listRestaurants.jade', {
             restaurants: rs
@@ -45,9 +81,25 @@ module.exports = function(app) {
 
     app.get('/search/restaurants/category/is/:x', function(req, res) {
         var x = req.params.x
+        console.log(x)
+        if(x == "Fast-Food")
+            var y = "Fast Food"
+        else
+            var y = x
+
 
         // TODO: lookup restaurants belonging to category :x
-        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
+        //var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
+
+        var rs = _.filter(restaurants, function(chr){
+            for(var i = 0; i < chr.categories.length; i++){
+                console.log(chr.categories[i])
+                if( chr.categories[i] == y)
+                    return true
+                
+            }
+            return false
+        });
 
         res.render('listRestaurants.jade', {
             restaurants: rs
@@ -60,8 +112,13 @@ module.exports = function(app) {
         var relationship = req.params.relationship
 
         // TODO: lookup restaurants with starts higher or lower than :number
-        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
-
+        //var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
+        var rs = _.filter(restaurants, function(chr){
+            if(relationship == "above")
+                return (chr.stars >= number)
+            else if(relationship == "below")
+                return (chr.stars <= number)
+        })
         res.render('listRestaurants.jade', {
             restaurants: rs
         })
@@ -74,10 +131,58 @@ module.exports = function(app) {
         var category = req.query.category
         var ambience = req.query.ambience    
         
-        console.log('req.query: ', req.query)    
+        
+        console.log('req.query: ', req.query)
+        console.log('minStars', minStars)    
         
         // // TODO: lookup restaurants with the given query parameters
-        var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
+        //var rs = [restaurants[1], restaurants[2], restaurants[3]] // hardcoded fake results
+        var rs = _.filter(restaurants, function(chr){
+            var token = [true, true, true, true]
+
+            if(name != undefined){
+                if(chr.name.indexOf(name) == -1)
+                    token[0] = false
+                else
+                    token[0] = true
+            }
+
+            if(minStars != undefined){
+                if(chr.stars < minStars)
+                    token[1] = false
+            }
+
+            if(category != undefined){
+                var find = false
+                for(var i = 0; i < chr.categories.length; i ++){
+                    if(chr.categories[i] == category)
+                        find = true
+                }
+
+                if(find == false) token[2] = false;
+            }
+
+            if(ambience != undefined){
+                var find = [false, false]
+                for(var i in chr.attributes){
+                if(i == "Ambience"){
+                    find[0] = true;
+                    for(var j in chr.attributes[i]){
+                        if(j == ambience){
+                            find[1] = true
+                            token[3] = chr.attributes[i][j]
+                        }
+                    }
+                }
+
+                if( !(find[0] && find[1]) )
+                    token[3] = false // if not found
+
+                }
+            }
+
+            return (token[0]&&token[1]&&token[2]&&token[3])
+        });
 
         res.render('listRestaurants.jade', {
             restaurants: rs
